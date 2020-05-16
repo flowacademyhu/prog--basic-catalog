bb6af579ed995cd2163f1e37e7013f1d77494156
levee98 <leventet@t-email.hu>
2020-05-16 17:41:04 +0200

    add union.js

---
+++ part1/union.js
@@ -0,0 +1,25 @@
+// 10th sentence: union of two arrays
+
+const unionOfArrays = (arr1, arr2) => {
+  const union = [];
+
+  for (const element of arr1) {
+    union.push(element);
+  }
+  console.log(union);
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (!union.includes(arr2[i])) {
+      union.push(arr2[i]);
+    }
+  }
+  return union;
+};
+
+// const a0 = [1, 2, 3, 4, 5, 6];
+// const a1 = [2, 4, -6, 6, 7, 110, -1];
+// console.log(unionOfArrays(a0, a1));
+
+modules.export {
+   unionOfArrays: union
+ };
