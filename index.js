f34917079f627a04ca6437515e661bb00a8702f0
GG_Juraj <gyorgy.galfalvy@gmail.com>
2020-05-16 17:16:40 +0200

    adding exports

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const bubbleSort = require('./bubbleSort');
 const insertionSort = require('./insertionSort');
 const minimumSort = require('./minimumSort');
 
+module.exports = {
+  bubbleSort: bubbleSort,
+  insertionSort: insertionSort,
+  minimumSort: minimumSort
+};
